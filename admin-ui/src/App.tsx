985151c9001bf59f945528e3d38c2c805364ad78
venkateswarlu kareti <venky@radiantsage.com>
2025-10-18 11:35:08 +0530

    feat: add global snackbar for notifications

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -8,11 +8,13 @@ import { Players } from './pages/Players'
 import { Courts } from './pages/Courts'
 import { Matches } from './pages/Matches'
 import { Registrations } from './pages/Registrations'
+import { GlobalSnackbar } from './components/GlobalSnackbar'
 
 export default function App() {
   return (
     <>
       <CssBaseline />
+      <GlobalSnackbar />
       <Routes>
         <Route path="/login" element={<Container maxWidth="sm"><Login /></Container>} />
         <Route element={<RequireAuth><Layout /></RequireAuth>}>
